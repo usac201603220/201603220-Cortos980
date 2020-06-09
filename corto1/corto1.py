e4894bde3ce848357f6ba90cae0d45cf1c62961b
usac201603220 <3013746000101@ingenieria.usac.edu.gt>
2020-06-09 12:38:14 -0600

    Entrega de examen corto

---
+++ corto1/corto1.py
@@ -0,0 +1,29 @@
+#Secuencia Collatz
+#Carnet 201603220
+
+
+def numero_par():
+    par = 0
+
+    if par == i:
+        par = par//2
+    return par
+
+def numero_impar():
+    impar = 0
+
+    if impar == i:
+        impar = (3*impar) + 1
+
+
+
+for i in range(1, 220): #se establece el rango del ciclo for
+    if i%2 != 0:
+        numero_impar
+    else:
+        numero_par
+
+        print(numero_par)
+        print(numero_impar)
+
+
